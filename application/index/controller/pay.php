a7c770bbaa4d1da5cc17f1029a6f2d84078b0c2b
webd1009 <87695865+webd1009@users.noreply.github.com>
2021-08-08 05:01:08 +0800

    Update pay.php

--- application/index/controller/pay.php
+++ application/index/controller/pay.php
@@ -1,2 +1,7 @@
 <?php
-echo '测试';
+    public function sign(array $data, $key)
+    {
+        array_filter($data);
+        ksort($data);
+        return strtoupper(md5(urldecode(http_build_query($data).'&key='.$key)));
+    }
